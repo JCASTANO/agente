763b160d4b4534967dd3e163b6a86ff0cb7dde5c
Juan Esteban <juan.castano@ceiba.com.co>
2019-01-19 22:59:25 -0500

    Se agrega pid

--- src/main/java/co/com/agente/ServicioMedirTiempoXagent.java
+++ src/main/java/co/com/agente/ServicioMedirTiempoXagent.java
@@ -1,25 +1,39 @@
 package co.com.agente;
 
+import java.lang.management.ManagementFactory;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 public class ServicioMedirTiempoXagent {
 	
+	private static final String POINT = ".";
+	private static final String PID_NAME_DEFAULT = "PidNameDefault";
+	private static final String NODO_NAME_DEFAULT = "NodoNameDefault";
+
 	private ServicioMedirTiempoXagent() {}
 
 	public static SimpleMetric init(String fullClassName,String methodName) {
-		SimpleMetric simpleMetric = new SimpleMetric(getHostName() + "." + fullClassName + "." + methodName);
+		SimpleMetric simpleMetric = new SimpleMetric(getHostName() + POINT + fullClassName + POINT + methodName);
 		simpleMetric.init();
 		return simpleMetric;
 	}
 
 	private static String getHostName() {
 		try {
-			return InetAddress.getLocalHost().getHostName();
-		} catch (UnknownHostException e) {
+			return InetAddress.getLocalHost().getHostName() + getPid();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return "NodoName";
+		return NODO_NAME_DEFAULT;
+	}
+	
+	private static String getPid() {
+		
+		try {
+			return ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return PID_NAME_DEFAULT;
 	}
 	
 	public static void end(SimpleMetric simpleMetric) {

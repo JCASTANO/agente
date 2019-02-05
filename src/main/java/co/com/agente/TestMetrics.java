a1f13a00eb2efbd010870820d9e16e9427772e5d
Juan Esteban <juan.castano@ceiba.com.co>
2019-02-04 19:30:26 -0500

    Se adiciona clase al agente

--- src/main/java/co/com/agente/TestMetrics.java
+++ src/main/java/co/com/agente/TestMetrics.java
@@ -5,7 +5,7 @@ public class TestMetrics {
 	public static void main(String[] args) throws InterruptedException {
 		System.out.println("inicio");
 		SimpleMetric simpleMetric = ServicioMedirTiempoXagent.init("co/com/sura.ServicioUsuario", "login");
-		Thread.sleep(1000);
+		Thread.sleep(40000);
 		ServicioMedirTiempoXagent.end(simpleMetric);
 		System.out.println("fin");
 	}
